2d2b68f1401e3611385188a3b4038f50935f1ecf
Eric J. Duran <eric.duran7@gmail.com>
2012-05-04 00:56:38 -0400

    Adding delete, help, images

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
 
 // Doc.Ready. Render our todos
 todo.renderAll();
-
+keys.prepare();
 // Attach save
-form = document.getElementById("action");
+saveB = document.getElementById("action-submit");
 textInput = document.getElementById('text')
-form.addEventListener('submit', function(event) {
+saveB.addEventListener('click', function(event) {
   event.preventDefault();
   todo.add(textInput.value);
   todo.clear();
   todo.renderAll();
-  console.log('fired');
+  // Only do this if todo.add() returns succesful.
+  textInput.value = '';
+});
+
+$("#text").keyup(function(event){
+    if(event.keyCode == 13){
+      event.preventDefault();
+      todo.add(textInput.value);
+      todo.clear();
+      todo.renderAll();
+      // Only do this if todo.add() returns succesful.
+      textInput.value = '';
+    }
+});
+
+$('li').swipeRight(function(){
+  // Remove the current li that was swipe
+  todo.remove($(this).parent())
+  $(this).remove();
+});
+
+$('#list').on('click', '.icon-remove', function (event) {
+  var $item = $(this).parent();
+  todo.remove($item.data('key'));
+  // Only remove if todo.remove is TRUE.
+  $item.remove();
 });
